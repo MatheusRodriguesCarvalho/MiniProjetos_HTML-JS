0700893d68fa1b63111d07eb86adb3144c26c06a
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-09-16 13:37:16 -0300

    Update index.js
    Even if it lacks some important things

---
+++ Sphere Shooter/index.js
@@ -0,0 +1,98 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth-10
+canvas.height = innerHeight-10
+
+
+// define a Class to the player
+class Player {
+	// this one is going to set and define the essntials properties of the player
+	constructor(x, y, radius, color) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+	}
+	// this will efectvily create the player, showing it on the screen
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+}
+
+
+class Projectile {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+	// the UPDATE ones are going do make the object move on the screen, the player dosen't have it
+	// because it is stuck in the middle of the screen
+	update() {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
+
+class Enemy {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+	update() {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
+
+const friction = 0.99
+class Particle {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+		this.alpha = 1
+	}
+	draw() {
+		c.save()
+		c.globalAlpha = this.alpha
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+		c.restore()
+	}
+	update() {
+		this.draw()
+		this.velocity.x *= friction
+		this.velocity.y *= friction
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+		this.alpha -= 0.01
+	}
+}
