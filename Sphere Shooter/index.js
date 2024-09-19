a653ffebc9e2cdff0db6c76cfa48f01b4bda2116
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-09-19 08:54:02 -0300

    Update index.js

--- Sphere Shooter/index.js
+++ Sphere Shooter/index.js
@@ -24,6 +24,79 @@ class Player {
 }
 
 
+class Projectile {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+	// the UPDATE ones are going do make the object move on the screen, the player dosen't have it
+	// because it is stuck in the middle of the screen
+	update() {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
+
+class Enemy {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+	update() {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
+
+const friction = 0.99
+class Particle {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+		this.alpha = 1
+	}
+	draw() {
+		c.save()
+		c.globalAlpha = this.alpha
+		c.beginPath()
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+		c.restore()
+	}
+	update() {
+		this.draw()
+		this.velocity.x *= friction
+		this.velocity.y *= friction
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+		this.alpha -= 0.01
+	}
+}
+
 // set the dimensions of the canvas, when the window may change its lenghts
 const x = canvas.width / 2
 const y = canvas.height / 2
@@ -192,78 +265,3 @@ StartGameBtn.addEventListener('click', () => {
 	
 	modalEL.style.display = 'none'
 })
-
-
-
-class Projectile {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x
-		this.y = y
-		this.radius = radius
-		this.color = color
-		this.velocity = velocity
-	}
-	draw() {
-		c.beginPath()
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-		c.fillStyle = this.color
-		c.fill()
-	}
-	// the UPDATE ones are going do make the object move on the screen, the player dosen't have it
-	// because it is stuck in the middle of the screen
-	update() {
-		this.draw()
-		this.x = this.x + this.velocity.x
-		this.y = this.y + this.velocity.y
-	}
-}
-
-class Enemy {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x
-		this.y = y
-		this.radius = radius
-		this.color = color
-		this.velocity = velocity
-	}
-	draw() {
-		c.beginPath()
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-		c.fillStyle = this.color
-		c.fill()
-	}
-	update() {
-		this.draw()
-		this.x = this.x + this.velocity.x
-		this.y = this.y + this.velocity.y
-	}
-}
-
-const friction = 0.99
-class Particle {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x
-		this.y = y
-		this.radius = radius
-		this.color = color
-		this.velocity = velocity
-		this.alpha = 1
-	}
-	draw() {
-		c.save()
-		c.globalAlpha = this.alpha
-		c.beginPath()
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-		c.fillStyle = this.color
-		c.fill()
-		c.restore()
-	}
-	update() {
-		this.draw()
-		this.velocity.x *= friction
-		this.velocity.y *= friction
-		this.x = this.x + this.velocity.x
-		this.y = this.y + this.velocity.y
-		this.alpha -= 0.01
-	}
-}
